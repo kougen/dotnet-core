d6cd3296ab7fae7cbc4c0da2bc86243ecb61daa8
Joshua Hegedus <jhegedus9@gmail.com>
2023-04-06 10:34:36 +0200

    fixed push.ps1

--- Modules/Tools/pack.ps1
+++ Modules/Tools/pack.ps1
@@ -3,8 +3,8 @@ $nuget_api_key = Get-Content ./.cred-nuget
 
 # Write-Output($api_key)
 
-# dotnet nuget push "../bin/Release/joshika39-Implementation.0.0.4.nupkg"  --api-key $git_api_key --source "github"
-# dotnet nuget push "../bin/Release/joshika39-Infrastructure.0.0.4.nupkg"  --api-key $git_api_key --source "github"
+dotnet nuget push "../bin/Release/joshika39-Implementation.0.0.4.nupkg"  --api-key $git_api_key --source "github"
+dotnet nuget push "../bin/Release/joshika39-Infrastructure.0.0.4.nupkg"  --api-key $git_api_key --source "github"
 
 dotnet nuget push "../bin/Release/joshika39-Implementation.0.0.4.nupkg" --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
 dotnet nuget push "../bin/Release/joshika39-Infrastructure.0.0.4.nupkg" --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
