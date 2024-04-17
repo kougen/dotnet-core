23f44dd74d0803863416ba5b60fa17b42492d4fc
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-17 23:57:28 +0200

    added named RaiseTick

--- _src/Infrastructure/Time/Listeners/ITickListener.cs
+++ _src/Infrastructure/Time/Listeners/ITickListener.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Time.Listeners
     public interface ITickListener
     {
         void RaiseTick(int round);
-        
+        void RaiseTick(IStopwatch initiator, string name, int round);
         TimeSpan ElapsedTime { get; set; }
     }
 }
